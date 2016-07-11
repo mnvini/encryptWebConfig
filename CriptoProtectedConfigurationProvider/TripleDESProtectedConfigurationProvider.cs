8f84aebc77a90fec30a97f39833f04cac1ead98e
mnvini <viniciusrcr@hotmail.com>
2016-07-11 20:30:58 -0300

    First Commit

---
+++ CriptoProtectedConfigurationProvider/TripleDESProtectedConfigurationProvider.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
+namespace CriptoProtectedConfigurationProvider
+{
+    public class TripleDESProtectedConfigurationProvider : ProtectedConfigurationProvider
+    {
+        private TripleDESCryptoServiceProvider tripleDesCripto = new TripleDESCryptoServiceProvider();
+        private string key = "Sgo0k8dv112TCU0BiMNOo0tfR5AYmXeC";
+        private string IV = "mmVbNIIj8k4=";
+
+        public TripleDESProtectedConfigurationProvider()
+        {
+            //Read the key and IV
+            tripleDesCripto.Key = Convert.FromBase64String(key);
+            tripleDesCripto.IV = Convert.FromBase64String(IV);
+            //ECB(Electronic code Book)
+
+            tripleDesCripto.Mode = CipherMode.ECB;
+            //padding mode if have any additional byte to be added
+            tripleDesCripto.Padding = PaddingMode.PKCS7;
+        }
+
+
+        public override XmlNode Encrypt(XmlNode node)
+        {
+
+            string encryptedData = EncryptString(node.OuterXml);
+
+            //Create section, or Web Config Encrypted
+                        XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.PreserveWhitespace = true;
+            xmlDoc.LoadXml("<EncryptedData>" + encryptedData + "</EncryptedData>");
+            return xmlDoc.DocumentElement;
+        }
+
+
+        public override XmlNode Decrypt(XmlNode encryptedNode)
+        {
+            string decryptedData = DecryptString(encryptedNode.InnerText);
+
+            // Decrypts the section or the Web Config Encrypted
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.PreserveWhitespace = true;
+            xmlDoc.LoadXml(decryptedData);
+
+            return xmlDoc.DocumentElement;
+        }
+
+        /// <summary>
+        /// Method for Encrypting
+        /// </summary>
+        private string EncryptString(string encryptValue)
+        {
+
+            byte[] valBytes = Encoding.Unicode.GetBytes(encryptValue);
+
+            ICryptoTransform transform = tripleDesCripto.CreateEncryptor();
+
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, transform, CryptoStreamMode.Write);
+            cs.Write(valBytes, 0, valBytes.Length);
+            cs.FlushFinalBlock();
+
+            byte[] returnBytes = ms.ToArray();
+            cs.Close();
+
+            return Convert.ToBase64String(returnBytes);
+        }
+
+        /// <summary>
+        /// Method for Decrypt
+        /// </summary>
+        private string DecryptString(string encryptedValue)
+        {
+            byte[] valBytes = Convert.FromBase64String(encryptedValue);
+
+            ICryptoTransform transform = tripleDesCripto.CreateDecryptor();
+
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, transform, CryptoStreamMode.Write);
+            cs.Write(valBytes, 0, valBytes.Length);
+            cs.FlushFinalBlock();
+            byte[] returnBytes = ms.ToArray();
+            cs.Close();
+
+            return Encoding.Unicode.GetString(returnBytes);
+        }
+    }
+}
---
+++ CritptoFiles/Program.cs
@@ -0,0 +1,78 @@
+﻿using CriptoProtectedConfigurationProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace CritptoFiles
+{
+    class Program
+    {
+        static TripleDESProtectedConfigurationProvider provider = new TripleDESProtectedConfigurationProvider();
+
+        static void Main(string[] args)
+        {
+
+
+            var path = String.Empty;
+            while (String.IsNullOrEmpty(path))
+            {
+                Console.WriteLine("Selecione o caminho do Web.config");
+                path = Console.ReadLine();
+            }
+
+            ProtectedConfiguration(path);
+            Console.WriteLine("Web Config criptografado com sucesso");
+
+            Console.Read();
+        }
+
+        private static void ProtectedConfiguration(string path)
+        {
+
+            XmlDocument doc = new System.Xml.XmlDocument();
+            doc.Load(path);
+
+            //Creates an encrypted Section
+            var xmlContent = @"<configProtectedData><providers><add name='TripleDESProtectedConfigurationProvider' type='CriptoProtectedConfigurationProvider.TripleDESProtectedConfigurationProvider, CriptoProtectedConfigurationProvider' /></providers></configProtectedData>";
+
+            XElement element = XDocument.Parse(xmlContent).Root;
+
+            var xmlProtected = new XmlDocument();
+            xmlProtected.LoadXml(element.ToString());
+            var nodeProtected = xmlProtected.FirstChild;
+
+            XmlNode nodeConfiguration = doc.SelectSingleNode("/configuration");
+            var importNodeProvider = doc.ImportNode(nodeProtected, true);
+            nodeConfiguration.InsertAfter(importNodeProvider, nodeConfiguration.FirstChild);
+
+
+            // Get the section that will be encrypted
+            XmlNode node = doc.SelectSingleNode("/configuration/connectionStrings");
+
+            //Encripta os dados do nó
+            var criptoNode = provider.Encrypt(node);
+
+            //Remove all childs nodes and attributes
+            node.RemoveAll();
+
+            //Import the encrypted node
+            var importNodeCripto = doc.ImportNode(criptoNode, true);
+
+            //Insert the node in connectionString
+            node.InsertAfter(importNodeCripto, node.FirstChild);
+
+
+            //Inserts the attribute reference to the provider configuration
+            XmlAttribute attr = doc.CreateAttribute("configProtectionProvider");
+            attr.Value = "TripleDESProtectedConfigurationProvider";
+            node.Attributes.Append(attr);
+
+            doc.Save(path);
+        }
+    }
+}
+
